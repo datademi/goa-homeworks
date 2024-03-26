4537be0c7732b8fa8ca9794114262e295a31de66
datademi <mrisxaneazeso@gmail.com>
2024-03-26 13:18:55 -0700

    commit

---
+++ day15/classwork/lists.py
@@ -0,0 +1,18 @@
+# list1 = ["nika keshelava", "data tezelashvili", "luka cxvaradze", "akaki sixarulidze" , "davit berkacashvili"]
+# list2 = ["audi","bmw","porshe","mersedes","perrari"]
+# print(list1)
+# print(list2)
+# print(list2[2])
+# print(list1[4])
+
+# Live
+
+list1 = ["game","books","language"]
+list2 = ["snake","minecraft"]
+print(len(list1))
+print(len(list2))
+
+surname = "dadiani"
+print(surname[-1])
+
+print(list1 + list2)
